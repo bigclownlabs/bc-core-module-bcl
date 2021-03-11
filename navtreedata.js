06ebc243a8a18eeadc66140f783b916c46ab034b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-11 07:51:42 +0000

    Deploy sdk.hardwario.com to github.com/hardwario/twr-sdk.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,34 @@
+var NAVTREE =
+[
+  [ "Firmware SDK", "index.html", [
+    [ "Overview", "index.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"group__twr__dice.html#ga64378ee1f2f8c0f55d27ae8b1ac2c3d3",
+"group__twr__gpio.html#ga89e53610bfca1171632775eebdd99347",
+"group__twr__module__co2.html#gac6e75a8ee3bd63b5f0a27db18891b5d4",
+"group__twr__onewire.html#gaec59dcd3d3dbc37ab40fce30fa139d75",
+"group__twr__sht20.html",
+"group__twr__tag__voc__lp.html#ga914c76f6188fbd9f49b0fbe1962befb0",
+"structtwr__esp8266__t.html#a05939917ae153fe5cf5f1e25b62aa922",
+"twr__button_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
